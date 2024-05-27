8851d996c1cf4e29a56b183b2d5c2be7c2093a4d
Luke Smith <larsin.218@gmail.com>
2024-05-27 04:07:22 -0600

    redoing everything because I definitely do not understand what I am doing :)

---
+++ main/server.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
